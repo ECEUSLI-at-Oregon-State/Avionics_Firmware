a4dfb5a71b4d8b3ee8aded0235beb03e54b3c826
Shirley Wong <wongsh@oregonstate.edu>
2020-02-21 14:08:41 -0800

    Change serial ports for new PCB

--- telemetry_unit_transmit/telemetry_unit_transmit.ino
+++ telemetry_unit_transmit/telemetry_unit_transmit.ino
@@ -8,9 +8,9 @@
 SdFs sd;
 TinyGPS gps;
 
-#define gpsPort Serial2
+#define gpsPort Serial1
 #define GPSECHO  true
-#define xBeeSerial Serial1
+#define xBeeSerial Serial3
 
 #define PMTK_SET_NMEA_UPDATE_10HZ "$PMTK220,100*2F"
 
@@ -79,17 +79,10 @@ void loop() {
 
   enumerate += 1;
   message = String(enumerate);
-  //
-  //  float pascals = baro.getPressure();
-  //  // Our weather page presents pressure in Inches (Hg)
-  //  // Use http://www.onlineconversion.com/pressure.htm for other units
-  //  float inHg = pascals / 3377;
-  //
+ 
   float altm = baro.getAltitude();
   float altmImperial = altm * 3.28084;
-
-  //    message = "\0";
-  //  message = "Alt: ";
+  
   message = String(message + ",");
   message = message + String(altmImperial);
 
@@ -106,54 +99,21 @@ void loop() {
     }
   }
 
-//  int year;
-//  uint8_t month, day, hour, minutes, second, hundredths;
   unsigned long age;
   float flat, flon;
 
   gps.f_get_position(&flat, &flon, &age);
   temp = get_datetime(gps, age);
   message = String(message + temp);
-//  //  gps.crack_datetime(&year, &month, &day, &hour, &minutes, &second, &hundredths, &age);
-//
-//  //  message = String(message + ",Time: ");
-//  //  message = String(message + ",");
-//  //
-//  //  temp = String(year);
-//  //  temp = String(temp + "/");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(month);
-//  //  temp = String(temp + "/");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(day);
-//  //  temp = String(temp + " ");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(hour);
-//  //  temp = String(temp + ":");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(minutes);
-//  //  temp = String(temp + ":");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(second);
-//  //  temp = String(temp + ".");
-//  //  message = String(message + temp);
-//  //
-//  //  temp = String(hundredths);
-//  //  message = String(message + temp);
-//
-  //  message = String(message + ",Lat: ");
+
+  // Latitude
   message = String(message + ",");
-  temp = String(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);//Latitude
+  temp = String(flat == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flat, 6);
   message = String(message + temp);
 
-  //  message = String(message + ",Lon: ");
+  // Longitude
   message = String(message + ",");
-  temp = String(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);//Longitude
+  temp = String(flon == TinyGPS::GPS_INVALID_F_ANGLE ? 0.0 : flon, 6);
   message = String(message + temp);
 
   readAccel();
