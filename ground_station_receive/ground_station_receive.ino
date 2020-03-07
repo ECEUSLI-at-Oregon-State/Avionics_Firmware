9f3d974c765a9168e2d1e321bfc1ccfce6e2655a
Shirley Wong <wongsh@oregonstate.edu>
2020-03-07 10:27:06 -0800

    Add back logging, fix GPS
    - Added back microSDc card functionality
    - New GPS library that works for this sensor
    - Changed log names for fullscale2

--- ground_station_receive/ground_station_receive.ino
+++ ground_station_receive/ground_station_receive.ino
@@ -4,8 +4,13 @@
 
 #include <stdio.h>
 #include <string.h>
+#include "SdFat.h"
+SdFs sd;
 
 #define xBeeSerial Serial3
+
+const int SD_CS_PIN = SDCARD_SS_PIN;
+FsFile dataFile;
 
 String received = "";
 String temp = "";
@@ -20,6 +25,11 @@ void setup() {
   xBeeSerial.begin(9600);
   while (!xBeeSerial) {};
 
+  while (!sd.begin(SdSpiConfig(SD_CS_PIN, DEDICATED_SPI, SD_SCK_MHZ(50)))) {
+    Serial.println("microSD Initialization Failed");
+    delay(1000);
+  }
+
   Serial.println("Initialized");
 }
 
@@ -29,18 +39,19 @@ void loop() {
   if (xBeeSerial.available()) {
     receive_data();
     Serial.println(received);
+    log_data();
 
     tokenize(received, fields);
 
-//    // Print fields
-//    for (int n = 0; n < 6; n++) {
-//      Serial.println(fields[n]);
-//    }
+    //    // Print fields
+    //    for (int n = 0; n < 6; n++) {
+    //      Serial.println(fields[n]);
+    //    }
 
     String check_checksum = reconstruct_msg(fields);
-//    Serial.println(check_checksum);
+    //    Serial.println(check_checksum);
     if (verify_checksum(check_checksum, fields)) {
-      Serial.println("ACK: " + String(fields[0]) + "!\n");
+//      Serial.println("ACK: " + String(fields[0]) + "!\n");
       xBeeSerial.println("ACK: " + String(fields[0]) + "!");
     }
 
@@ -49,6 +60,16 @@ void loop() {
     received = "";
   }
   delay(10);
+}
+
+void log_data() {
+  dataFile = sd.open("Fullscale2-3-8-20-gs.txt", FILE_WRITE);
+  if (dataFile) {
+    dataFile.println(received);
+    dataFile.close();
+  } else {
+    Serial.println("Couldn't Write to File.");
+  }
 }
 
 void receive_data() {
@@ -88,7 +109,7 @@ String reconstruct_msg(String fields[]) {
 
   // Rebuild message string to calculate checksum with
   for (i = 0; i < 8; i++) {
-//    Serial.println(fields[i]);
+    //    Serial.println(fields[i]);
     message += fields[i];
   }
   return message;
@@ -102,8 +123,8 @@ bool verify_checksum(String msg, String fields[]) {
     }
   }
   checksum %= 256;
-  Serial.println("Calculated " + String(checksum));
-  Serial.println("Comparing with " + fields[8]);
+//  Serial.println("Calculated " + String(checksum));
+//  Serial.println("Comparing with " + fields[8]);
   if (checksum == fields[8].toInt()) {
     return true;
   }
